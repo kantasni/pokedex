424a5f20b1eafe88e34316960edba100d453c24d
DimitrisNikoltsanis <nikoltsanis@gmail.com>
2021-07-17 14:25:07 +0300

    Before images

--- src/main/java/gr/pokedex/demo/controllers/PokemonController.java
+++ src/main/java/gr/pokedex/demo/controllers/PokemonController.java
@@ -15,26 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class PokemonController {
 
     @Autowired
-    private PokemonRepository pokemonRepository;
-
-    @Autowired
     private PokemonService pokeservice;
 
     @GetMapping(value = "/uncaught")
     public List<Pokemon> getPokemons() {
-        return pokemonRepository.findAllByCaught(false);
+        return pokeservice.fetchAllByCaught(false);
     }
 
     @GetMapping(value = "/create")
     public void createPokemon(@RequestBody Pokemon pokemon) {
-        pokemonRepository.save(pokemon);
+        pokeservice.addPokemon(pokemon);
     }
 
     @GetMapping(value = "/catch")
-    public void catchPokemon(@RequestBody String name) { pokeservice.catchPokemon(name); }
+    public boolean catchPokemon(@RequestBody String name) {return pokeservice.catchPokemon(name); }
 
     @GetMapping(value = "/caught")
     public  List<Pokemon> getCaught() {
-        return pokemonRepository.findAllByCaught(true);
+        return pokeservice.fetchAllByCaught(true);
     }
 }
--- src/main/java/gr/pokedex/demo/services/PokemonService.java
+++ src/main/java/gr/pokedex/demo/services/PokemonService.java
@@ -5,17 +5,32 @@ import gr.pokedex.demo.repositories.PokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class PokemonService {
 
     @Autowired
     private PokemonRepository pokemonRepository;
 
-    public void catchPokemon(String pokemonName) {
-        Pokemon caughtPokemon = pokemonRepository.findByName(pokemonName);
-        caughtPokemon.setCaught(true);
+    public boolean catchPokemon(String pokemonName) {
+        Random rd = new Random();
 
-        Pokemon save = pokemonRepository.save(caughtPokemon);
+        if(rd.nextBoolean()) {
+            Pokemon caughtPokemon = pokemonRepository.findByName(pokemonName);
+            caughtPokemon.setCaught(true);
+            return pokemonRepository.save(caughtPokemon)!=null;
+        }
+        return false;
     }
-//    TODO: create methods for /uncaught, /create and /caught
+
+    public List<Pokemon> fetchAllByCaught(boolean areCaught) {
+        return pokemonRepository.findAllByCaught(areCaught);
+    }
+
+    public void addPokemon(Pokemon pokemon){
+        pokemonRepository.save(pokemon);
+    }
+
 }
