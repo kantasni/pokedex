7b2c756c4752d44275b375759b39681f46ea5be9
n.kantas <n.kantas@netmechanics.gr>
2021-07-18 14:16:20 +0300

    initial

--- src/main/java/gr/pokedex/demo/controllers/ImageController.java
+++ src/main/java/gr/pokedex/demo/controllers/ImageController.java
@@ -1,15 +1,12 @@
 package gr.pokedex.demo.controllers;
 
 import gr.pokedex.demo.entities.PokemonImage;
-import gr.pokedex.demo.repositories.ImageRepository;
 import gr.pokedex.demo.services.ImageService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
